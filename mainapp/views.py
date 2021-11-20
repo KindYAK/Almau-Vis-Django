a2b57edc29c228e987a09fb0c60b634f6c8ebcc7
Kirill  Yakunin <kirill.yakunin@toptal.com>
2021-11-20 10:57:04 +0600

    feat - server-side Seaborn - cache

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,7 @@
 import datetime
 import os
 import random
+import time
 
 import pandas as pd
 import seaborn as sns
@@ -20,24 +21,34 @@ class OrderListView(ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(object_list=object_list, **kwargs)
         context['len_clients'] = Client.objects.count()
-        qs = Category.objects.filter(product__order__in=self.get_queryset())
-        qs = qs.annotate(
-            order_count=Count('product__order'),
-            average_price=Avg('product__order__price')
-        )
 
-        plot = sns.barplot(data=pd.DataFrame([
-            {
-                "name": i.name,
-                "order_count": i.order_count
-            } for i in qs
-        ]), x="name", y="order_count")
-
-        filename = f"bar-plot-order-count-{str(datetime.datetime.now()).replace(':', '-')}-{random.randint(1, 100000)}.jpg"
+        filename = f"bar-plot-order-count-{str(dict(self.request.GET)).replace(':', '=').replace('<', '(').replace('>', ')')}.jpg"
         path = os.path.join(
             MEDIA_ROOT,
             filename
         )
-        plot.get_figure().savefig(path)
         context['filename'] = MEDIA_URL + "/" + filename
+
+        try:
+            cr = datetime.datetime.fromtimestamp(
+                os.path.getmtime(path)
+            )
+            file_age = (datetime.datetime.now() - cr).seconds
+        except:
+            file_age = 100 * 60 * 24
+
+        if file_age > 1:
+            qs = Category.objects.filter(product__order__in=self.get_queryset())
+            qs = qs.annotate(
+                order_count=Count('product__order'),
+                average_price=Avg('product__order__price')
+            )
+
+            plot = sns.barplot(data=pd.DataFrame([
+                {
+                    "name": i.name,
+                    "order_count": i.average_price
+                } for i in qs
+            ]), x="name", y="order_count")
+            plot.get_figure().savefig(path)
         return context
